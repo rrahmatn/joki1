71e036651f2fa0531c9099a772ecce84dd845a98
rrahmatn <rizkyrahmat292@gmail.com>
2023-12-31 18:22:06 +0700

    hore

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const cors = require('cors')
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const Product = require("./product"); // Pastikan file "product.js" mengandung definisi skema produk
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: '*', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-}));
+app.use(cors())
 
-mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://david:apaantuh11@pam.spi9mzl.mongodb.net/flutter", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb+srv://david:apaantuh11@pam.spi9mzl.mongodb.net/flutter", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
 
   .then(() => {
     console.log("Connected to MongoDB");
@@ -26,55 +26,46 @@ mongoose.connect("mongodb+srv://david:apaantuh11@pam.spi9mzl.mongodb.net/flutter
       try {
         let dataToStore = await data.save();
         res.status(200).json(dataToStore);
-
       } catch (error) {
         res.status(400).json({
-          'status': error.message
-        })
-
+          status: error.message,
+        });
       }
-    })
-    
-    app.get("/product", async (req, res) => {
+    });
 
+    app.get("/product", async (req, res) => {
       try {
         let data = await Product.find();
         res.status(200).json(data);
       } catch (error) {
-        res.status(500).json(error.message)
+        res.status(500).json(error.message);
       }
-    
-    })
-    
-    
-    app.patch("/product/:id", async (req, res) => {
+    });
 
+    app.patch("/product/:id", async (req, res) => {
       let id = req.params.id;
       let updatedData = req.body;
-      let options = {new: true};
+      let options = { new: true };
 
       try {
-        const data = await Product.findByIdAndUpdate(id,updatedData, options);
-      res.send(data);
+        const data = await Product.findByIdAndUpdate(id, updatedData, options);
+        res.send(data);
       } catch (error) {
         res.send(error.message);
       }
-    })
-    
-    app.delete("/product/:id", async (req, res) => {
+    });
 
+    app.delete("/product/:id", async (req, res) => {
       let id = req.params.id;
       try {
         const data = await Product.findByIdAndDelete(id);
         res.json({
-          'status': "Deleted the product ${data.pname} from database"
+          status: "Deleted the product ${data.pname} from database",
         });
       } catch (error) {
         res.json(error.message);
       }
-
-    })
-
+    });
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error.message);
