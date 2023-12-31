26643827ca9ef1d56c40f63d689935a300753d5c
rrahmatn <rizkyrahmat292@gmail.com>
2023-12-31 20:23:43 +0700

    dasda

--- index.js
+++ index.js
@@ -10,71 +10,72 @@ mongoose
   .connect("mongodb+srv://david:apaantuh11@pam.spi9mzl.mongodb.net/flutter", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(() => {
+  })
+  .then(() => {
     console.log("Connected to MongoDB");
-
-    app.post("/product", async (req, res) => {
-      console.log("Result", req.body);
-
-      let data = Product(req.body);
-
-      try {
-        let dataToStore = await data.save();
-        res.status(200).json(dataToStore);
-      } catch (error) {
-        res.status(400).json({
-          status: error.message,
-        });
-      }
-    });
-
-    app.get("/", (req, res) => {
-      res.send("Express on Vercel");
-    });
-
-    app.get('/api', (req, res) => {
-      res.setHeader('Content-Type', 'text/html');
-      res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-      res.end(`Hello! Go to item: <p>hello</p>`);
-    });
-
-    app.get("/product", async (req, res) => {
-      try {
-        let data = await Product.find();
-        res.status(200).json(data);
-      } catch (error) {
-        res.status(500).json(error.message);
-      }
-    });
-
-    app.patch("/product/:id", async (req, res) => {
-      let id = req.params.id;
-      let updatedData = req.body;
-      let options = { new: true };
-
-      try {
-        const data = await Product.findByIdAndUpdate(id, updatedData, options);
-        res.send(data);
-      } catch (error) {
-        res.send(error.message);
-      }
-    });
-
-    app.delete("/product/:id", async (req, res) => {
-      let id = req.params.id;
-      try {
-        const data = await Product.findByIdAndDelete(id);
-        res.json({
-          status: `Deleted the product ${data.pname} from database`,
-        });
-      } catch (error) {
-        res.json(error.message);
-      }
-    });
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error.message);
   });
+
+app.post("/product", async (req, res) => {
+  console.log("Result", req.body);
+
+  let data = Product(req.body);
+
+  try {
+    let dataToStore = await data.save();
+    res.status(200).json(dataToStore);
+  } catch (error) {
+    res.status(400).json({
+      status: error.message,
+    });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+app.get("/api", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <p>hello</p>`);
+});
+
+app.get("/product", async (req, res) => {
+  try {
+    let data = await Product.find();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+app.patch("/product/:id", async (req, res) => {
+  let id = req.params.id;
+  let updatedData = req.body;
+  let options = { new: true };
+
+  try {
+    const data = await Product.findByIdAndUpdate(id, updatedData, options);
+    res.send(data);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+app.delete("/product/:id", async (req, res) => {
+  let id = req.params.id;
+  try {
+    const data = await Product.findByIdAndDelete(id);
+    res.json({
+      status: `Deleted the product ${data.pname} from database`,
+    });
+  } catch (error) {
+    res.json(error.message);
+  }
+});
 
 app.listen(3000, () => {
   console.log("Connected to server at 3000");
